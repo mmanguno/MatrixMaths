862fa82e0e77b2e4709dae5efa5f1d72e8fce909
mmanguno <mmanguno3@gatech.edu>
2014-11-07 12:35:27 -0500

    Changes some documentation on matrixmultiply.py

--- rudiments/matrixmultiply.py
+++ rudiments/matrixmultiply.py
@@ -1,15 +1,28 @@
+#!/usr/bin/env python
+"""Contains functions that multiply matrices together"""
+
 import numpy
+
+__author__ = "Jayden Gardiner"
+__version__ = "1.0"
+__date__ = "2014 November 7"
+__credits__ = ["Mitchell Manguno"]
+
+__maintainer__ = "Jayden Gardiner"
+__email__ = "jgardiner@gatech.edu"
+__status__ = "Prototype"
 
 def multiply(x, y):
 	"""
 	This function will multiply two matrices.
 
-	This function to multiply two matrices will
-	return a new matrix after iterating
-	through the rows and columns of the
-	matrices and calculating the new values.
+	This function to multiply two matrices will return a new matrix after
+    iterating through the rows and columns of the matrices and calculating the 
+    new values.
 
-	x, y: Matrices
+    Keyword arguments:
+    x -- the first matrix to multiply
+	y -- the second matrix to multiply
 	"""
     #Type checking x and y as numpy matrices
     if x !(isInstance(matrix, numpy)):
