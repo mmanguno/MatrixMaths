ef2ba3a27e8eaef32076c38acd08252801e106e5
JaydenGardiner <jaydengardiner@live.com>
2014-11-11 12:16:34 -0500

    Adds matrix multiplication and Givens rotations.
    Adds file to matrix multiply as well as work-in-progress Givens rotation
    code.

---
+++ matrixmaths/matrix_multiplication.java
@@ -0,0 +1,66 @@
+package rudiments;
+
+import Jama.Matrix;
+
+/**
+ * Includes method to multiply two matrices
+ * Math 2605
+ * @author Jayden Gardiner
+ * @version 1.0
+ */
+public class matrix_multiplication {
+
+    /**
+     * This method multiplies two matrices and
+     * returns a new matrix after iterating
+     * through the rows and columns of the
+     * matrices and calculating the new values.
+     * @param x Matrix to be multiplied by matrix y
+     * @param y Matrix to be multiplied by matrix x
+     * @return z New multiplied matrix
+     */
+    public static Matrix multiply(Matrix x, Matrix y) {
+        double[][] z = new double[x.length - 1][b[0].length - 1];
+        Matrix z = new Matrix(z);
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < y[0].length; j++) {
+                for (int k = 0; k < x[0].length; k++) {
+                    z[i][j] += x[i][k] * y[k][j]
+                }
+            }
+        }
+        return z;
+    }
+    
+    /**
+     * This method takes in two 2D arrays, turns them
+     * into Jawa matrices, then multiplying two
+     * matrices will return a new matrix after
+     * iterating through the rows and columns of the
+     * matrices and calculating the new values.
+     * @param x Matrix to be multiplied by matrix y
+     * @param y Matrix to be multiplied by matrix x
+     * @return New multiplied matrix
+     */
+    public static Matrix multiply(double[][] x, double[][] y) {
+        Matrix x = new Matrix(x);
+        Matrix y = new Matrix(y);
+        return multiply(x, y);
+    }
+    
+    public static Matrix multiply(Matrix[] matrices) {
+        double[][] z = new double[matrices.length - 1][matrices.length - 1];
+        Matriz z = new Matrix(z);
+        for (int b = 1; b < matrices[0].length; b++) {
+            for (int i = 0; i < matrices[0] - b; i++) {
+                int j = i + b;
+                z[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    z[i][j] = Math.min(z[i, j], z[i][k] + z[k + 1][j]
+                            + matrices[i]*matrices[k + 1]*matrices[j + 1]]);
+                }
+            }
+        }
+        return z;
+    }
+}
---
+++ matrixmaths/qr_fact_givens.java
@@ -0,0 +1,61 @@
+package rudiments;
+
+import Jama.Matrix;
+
+/**
+ * Includes method to calculate Givens rotation
+ * Math 2605
+ * @author Jayden Gardiner
+ * @version 1.0
+ */
+public class qr_fact_givens {
+    
+    Matrix Q;
+    Matrix R;
+    
+    public Givens(double[][] A) {
+        Matrix matrix = new Matrix(A);
+        Givens(matrix);
+    }
+    
+    public Givens(Matrix A) {
+        double[][] newMatrix = new double[A.length - 1][A[0].length - 1];
+        Matrix Gn = new Matrix(newMatrix);
+        Matrix An = new Matrix(newMatrix);
+        //form identity matrix
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                if (i == j) {
+                    Gn[i][j] = 1;
+                } else {
+                    Gn[i][j] = 0;
+                }
+            }
+        }
+        //calc cos + sin shit
+        for (int i = 0; i < A.length; i++) {
+            for (int j  = A.length - 1; j > 1; j--) {
+                double x = A[j - 1][i];
+                double y = A[j][i];
+                double cosTheta = x / (Math.sqrt(x * x + y * y));
+                double sinTheta = y / (Math.sqrt(x * x + y * y));
+                Gn[j][j] = cosTheta;
+                Gn[j][j - 1] = sinTheta;
+                Gn[j - 1][j] = -sinTheta;
+                Gn[j - 1][j - 1] = cosTheta;
+                Matrix GnA = multiply(Gn, A);
+                Q = multiply(Gn.transpose(), Q);
+                R = multiply(Gn, R);
+                //....
+            }
+        }
+    }
+    
+    public static Matrix getQ() {
+        return Q;
+    }
+    
+    public static Matrix getR() {
+        return R;
+    }
+}
