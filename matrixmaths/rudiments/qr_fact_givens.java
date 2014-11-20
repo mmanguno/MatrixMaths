45e395b3f26f02fd0de48042ae180c4f795274a3
JaydenGardiner <jaydengardiner@live.com>
2014-11-19 21:04:27 -0500

    Edit

--- matrixmaths/rudiments/qr_fact_givens.java
+++ matrixmaths/rudiments/qr_fact_givens.java
@@ -1,5 +1,7 @@
 package rudiments;
 
+import java.util.Arrays;
+
 import Jama.Matrix;
 
 /**
@@ -24,8 +26,12 @@ public class qr_fact_givens {
         }
         
         double[][] An = A;
-        double[][] Gn = new double[A.length - 1][A.length - 1];
-        double[][] Q = new double[A.length - 1][A.length - 1];
+        double[][] Gn = new double[A.length][A.length];
+        double[][] Q = new double[A.length][A.length];
+        
+        System.out.println(); 
+        System.out.println("Number of rows and columns in our original matrix: " + (A.length) + " x " + (A.length) + ".");
+        System.out.println();  
         
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A.length; j++) {
@@ -38,9 +44,11 @@ public class qr_fact_givens {
                 }
             }
         }
+        
+        int iteration = 1;
 
         for (int i = 0; i < A.length; i++) {
-            for (int j  = A.length - 1; j > 1; j--) {
+            for (int j  = A.length - 1; j > i + 1; j--) {
                 double x = An[j - 1][i];
                 double y = An[j][i];
                 double cosTheta = x / (Math.sqrt(x * x + y * y));
@@ -50,7 +58,14 @@ public class qr_fact_givens {
                 Gn[j - 1][j] = -sinTheta;
                 Gn[j - 1][j - 1] = cosTheta;
                 
+                System.out.println("G" + iteration + ":");
+                new Matrix(Gn).print(2,5);
+
                 An = matrix_multiplication.multiply2DArrays(Gn, An);
+                
+                System.out.println("A" + iteration + ":");
+                new Matrix(An).print(2, 5);
+                
                 Q = matrix_multiplication.multiply2DArrays(Gn, Q);
                 
                 for (int a = 0; a < A.length; a++) {
@@ -62,8 +77,15 @@ public class qr_fact_givens {
                         }
                     }
                 }
+                iteration += 1;
             }
         }
+        
+        System.out.println("Q:");
+        new Matrix(Q).print(2, 5);
+         
+        System.out.println("R:");
+        new Matrix(An).print(2, 5);
         
         Matrix Qmatrix = new Matrix(Q);
         Qmatrix = Qmatrix.transpose();
@@ -71,6 +93,21 @@ public class qr_fact_givens {
         Matrix Rmatrix = new Matrix(R);
         Matrix[] QR = {Qmatrix, Rmatrix};
         
+        System.out.println("QR:");
+        Matrix mm = matrix_multiplication.multiply(Q, R);
+        mm.print(2, 5);
+        System.out.println("A:");
+        new Matrix(A).print(2, 5);
+        
         return QR;
     }
+   
+   public static void printMatrix(double[][] matrix) {
+       System.out.println(Arrays.deepToString(matrix));
+   }
+   
+   public static void main(String[] args) {
+       double[][] hello = {{6, 5, 1},{5, 1, 1}, {1, 1,1}};
+       factorize(hello);
+   }
 }
